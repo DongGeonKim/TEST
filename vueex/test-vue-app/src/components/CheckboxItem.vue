df9ea577381bbaa8450f0fdfccc6ac0cc390577c
dgkim <dgkim@naruint.com>
2023-08-14 17:49:15 +0900

    test

--- vueex/test-vue-app/src/components/CheckboxItem.vue
+++ vueex/test-vue-app/src/components/CheckboxItem.vue
@@ -1,16 +1,12 @@
 <template>
 	<li>
-		<input type="checkbox" :checked="checked" /> 옵션1
+			<input type="checkbox" :checked="checked" /> {{name}}
 	</li>
 </template>
 
 <script>
-export default {
-	name: "CheckboxItem",
-	data() {
-		return {
-			checked: false
-		}
+	export default {
+			name : "CheckboxItem",
+			props : ["name", "checked"],
 	}
-}
 </script>
