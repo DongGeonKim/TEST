ba917bd2dad6324a64ea343d886f34cf1284bb42
dgkim <dgkim@naruint.com>
2023-09-05 14:20:21 +0900

    12

--- vueex/todolist-app-router/src/pages/TodoItem.vue
+++ vueex/todolist-app-router/src/pages/TodoItem.vue
@@ -1,17 +1,14 @@
 <template>
-    <li :class="todoItem.done ? 'list-group-item list-group-item-success' : 'list-group-item'">
-      <span :class="todoItem.done ? 'todo-done pointer' : 'pointer'"
-        @click="toggleDone(todoItem.id)">
-        {{todoItem.todo}}
-        {{todoItem.done ? '(완료)' : '' }}
-      </span>
-      <span class="float-end badge bg-secondary pointer m-1" 
-        @click="router.push(`/todos/edit/${todoItem.id}`)">
-        편집</span>
-      <span class="float-end badge bg-secondary pointer m-1" 
-        @click="deleteTodo(todoItem.id)">
-        삭제</span>
-    </li>
+	<li :class="todoItem.done ? 'list-group-item list-group-item-success' : 'list-group-item'">
+		<span :class="todoItem.done ? 'todo-done pointer' : 'pointer'" @click="toggleDone(todoItem.id)">
+			{{ todoItem.todo }}
+			{{ todoItem.done ? '(완료)' : '' }}
+		</span>
+		<span class="float-end badge bg-secondary pointer m-1" @click="router.push(`/todos/edit/${todoItem.id}`)">
+			편집</span>
+		<span class="float-end badge bg-secondary pointer m-1" @click="deleteTodo(todoItem.id)">
+			삭제</span>
+	</li>
 </template>
 
 <script setup>
@@ -19,7 +16,7 @@ import { useRouter } from 'vue-router';
 import { inject } from 'vue';
 
 defineProps({
-	todoItem: { Type: Object, required:true }
+	todoItem: { Type: Object, required: true }
 })
 
 const router = useRouter();
