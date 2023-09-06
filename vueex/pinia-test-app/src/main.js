321600a202c95a59b0c7eae257cad16f3aad76fb
dgkim <dgkim@naruint.com>
2023-09-06 15:37:20 +0900

    12

--- vueex/pinia-test-app/src/main.js
+++ vueex/pinia-test-app/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App1.vue'
+import { createApp } from "vue";
+import App from "./App1.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
