35fbaf9af1ab46550c229a788077c6c8761791b8
dgkim <dgkim@naruint.com>
2023-12-05 15:53:04 +0900

    12

--- Reactive_test_client/src/main/java/com/test/ReactiveTestClientApplication.java
+++ Reactive_test_client/src/main/java/com/test/ReactiveTestClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactiveExampleClientApplication {
+public class ReactiveTestClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactiveExampleClientApplication.class, args);
+		SpringApplication.run(ReactiveTestClientApplication.class, args);
 	}
 
 }
