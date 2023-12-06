3ab97d7a4d7994e678a95906184f62730c384fd2
dgk <csco0124@naver.com>
2023-12-07 00:26:34 +0900

    12

---
+++ Reactive_test/src/main/java/com/test/exam/Example7_1.java
@@ -0,0 +1,25 @@
+package com.test.exam;
+
+import java.util.Arrays;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+/**
+ * Cold Sequence 예제
+ */
+@Slf4j
+public class Example7_1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Flux<String> coldFlux =
+                Flux
+                    .fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
+                    .map(String::toLowerCase);
+
+        coldFlux.subscribe(country -> log.info("# Subscriber1: {}", country));
+        System.out.println("----------------------------------------------------------------------");
+        Thread.sleep(2000L);
+        coldFlux.subscribe(country -> log.info("# Subscriber2: {}", country));
+    }
+}
