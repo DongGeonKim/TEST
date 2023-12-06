f900aa9ebd964df59a169a3c1656ad20ee806d23
dgk <csco0124@naver.com>
2023-12-07 00:55:01 +0900

    12

--- Reactive_test/src/main/java/com/test/exam/Example7_1.java
+++ Reactive_test/src/main/java/com/test/exam/Example7_1.java
@@ -12,14 +12,13 @@ import reactor.core.publisher.Flux;
 public class Example7_1 {
     public static void main(String[] args) throws InterruptedException {
 
-        Flux<String> coldFlux =
-                Flux
-                    .fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
-                    .map(String::toLowerCase);
+        Flux<String> coldFlux = Flux
+        							.fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE"))
+                    				.map(String::toLowerCase);
 
         coldFlux.subscribe(country -> log.info("# Subscriber1: {}", country));
         System.out.println("----------------------------------------------------------------------");
-        Thread.sleep(2000L);
+        Thread.sleep(2000);
         coldFlux.subscribe(country -> log.info("# Subscriber2: {}", country));
     }
 }
